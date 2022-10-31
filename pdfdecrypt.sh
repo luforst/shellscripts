ed7a7830d0e41a1da6f660d51dd37a493daa5e3d
Lukas Forst <lukas.forst@zoho.com>
2022-10-31 09:31:05 +0100

    Add alternative command using qpdf

--- pdfdecrypt.sh
+++ pdfdecrypt.sh
@@ -3,3 +3,5 @@
 # decrypts a pdf file
 pdftk pdfdatei.pdf input_pw geheimespasswort output pdfdatei_decrypted.pdf 
 
+# alternative using qdf, works with a * in the password
+qpdf --password=paeaebO\*2022 --decrypt 2022.10.27\ -\ Vorlesung\ 2\ -\ Adaptive\ Immunität\ for\ PDF.pdf 2022.10.27\ -\ Vorlesung\ 2\ -\ Adaptive\ Immunität\ for\ PDF_decrypt.pdf
